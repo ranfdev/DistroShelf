2f16116a52ef87f09cca91c692df37defd863cb6
ranfdev <ranfdev@gmail.com>
2026-02-28 19:31:37 +0100

    trigger errors for clippy

--- .githooks/checks.sh
+++ .githooks/checks.sh
@@ -6,7 +6,7 @@ echo "• cargo fmt --all -- --check"
 cargo fmt --all -- --check
 
 echo "• cargo clippy --all-targets --all-features"
-cargo clippy --all-targets --all-features
+cargo clippy --all-targets --all-features -- -Dwarnings
 
 echo "• cargo test --all-features"
 cargo test --all-features
